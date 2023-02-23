99de50338108f6c6effbe03da84613526497f5e5
Bharakhda <nishaben.bharakhada@accenture.com>
2023-02-23 18:59:05 +0530

    second commit

--- demo/src/main/java/com/example/demo/TestApplication.java
+++ demo/src/main/java/com/example/demo/TestApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.io.PrintWriter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +12,9 @@ public class TestApplication {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("welcome to springboot!!");
 		
+		
+		PrintWriter out= new PrintWriter(System.out);
+		out.print("<h1>"+"Welcome to Springboot"+"</h1>");
 		
 	}
 
